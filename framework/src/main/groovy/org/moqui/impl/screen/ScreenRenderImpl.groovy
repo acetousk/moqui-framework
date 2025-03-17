445c1df7bbfe9d89de62e25db22d90bb99eea985
acetousk <acetousk@users.noreply.github.com>
2025-03-17 14:27:21 -0600

    Add getMenuDataNoUser

--- framework/src/main/groovy/org/moqui/impl/screen/ScreenRenderImpl.groovy
+++ framework/src/main/groovy/org/moqui/impl/screen/ScreenRenderImpl.groovy
@@ -2448,4 +2448,164 @@ class ScreenRenderImpl implements ScreenRender {
         // for (Map info in menuDataList) logger.warn("menu data item: ${info}")
         return menuDataList
     }
+    List<Map> getMenuDataNoUser(ArrayList<String> pathNameList) {
+//        if (!ec.user.userId) { ec.web.sendJsonError(401, "Authentication required", null); return null }
+        ScreenUrlInfo fullUrlInfo = ScreenUrlInfo.getScreenUrlInfo(this, rootScreenDef, pathNameList, null, 0)
+        if (!fullUrlInfo.targetExists) { ec.web.sendJsonError(404, "Screen not found for path ${pathNameList}", null); return null }
+        UrlInstance fullUrlInstance = fullUrlInfo.getInstance(this, null)
+        // if (!fullUrlInstance.isPermitted()) { ec.web.sendJsonError(403, "View not permitted for path ${pathNameList}", null); return null }
+        if (!fullUrlInstance.isPermitted()) { ec.logger.warn("View not permitted for path ${fullUrlInstance.url}") }
+
+        ArrayList<String> fullPathList = fullUrlInfo.fullPathNameList
+        int fullPathSize = fullPathList.size()
+        ArrayList<String> extraPathList = fullUrlInfo.extraPathNameList
+        int extraPathSize = extraPathList != null ? extraPathList.size() : 0
+        if (extraPathSize > 0) {
+            fullPathSize -= extraPathSize
+            fullPathList = new ArrayList<String>(fullPathList.subList(0, fullPathSize))
+        }
+
+        StringBuilder currentPath = new StringBuilder()
+        List<Map> menuDataList = new LinkedList<>()
+        ScreenDefinition curScreen = rootScreenDef
+
+        // to support menu titles with values set in pre-actions: run pre-actions for all screens in path except first 2 (generally webroot, apps)
+        ec.artifactExecutionFacade.setAnonymousAuthorizedView()
+        ec.userFacade.loginAnonymousIfNoUser()
+        ArrayList<ScreenDefinition> preActionSds = new ArrayList<>(fullUrlInfo.screenPathDefList.subList(2, fullUrlInfo.screenPathDefList.size()))
+        int preActionSdSize = preActionSds.size()
+        for (int i = 0; i < preActionSdSize; i++) {
+            ScreenDefinition sd = (ScreenDefinition) preActionSds.get(i)
+            if (sd.preActions != null) {
+                try { sd.preActions.run(ec) }
+                catch (Throwable t) { logger.warn("Error running pre-actions in ${sd.getLocation()} while getting menu data: " + t.toString()) }
+            }
+        }
+
+        for (int i = 0; i < (fullPathSize - 1); i++) {
+            String pathItem = (String) fullPathList.get(i)
+            String nextItem = (String) fullPathList.get(i+1)
+            currentPath.append('/').append(StringUtilities.urlEncodeIfNeeded(pathItem))
+
+            SubscreensItem curSsi = curScreen.getSubscreensItem(pathItem)
+            // already checked for exists above, path may have extra path elements beyond the screen so allow it
+            if (curSsi == null) break
+            curScreen = ec.screenFacade.getScreenDefinition(curSsi.location)
+
+            List<Map> subscreensList = new LinkedList<>()
+            ArrayList<SubscreensItem> menuItems = curScreen.getSubscreensItemsSorted()
+            int menuItemsSize = menuItems.size()
+            for (int j = 0; j < menuItemsSize; j++) {
+                SubscreensItem subscreensItem = (SubscreensItem) menuItems.get(j)
+
+                // include active subscreen even if not normally in menu
+                if (!subscreensItem.menuInclude && subscreensItem.name != nextItem) continue
+                // valid in current context? (user group, etc)
+                if (!subscreensItem.isValidInCurrentContext()) continue
+
+                String screenPath = new StringBuilder(currentPath).append('/').append(StringUtilities.urlEncodeIfNeeded(subscreensItem.name)).toString()
+                UrlInstance screenUrlInstance = buildUrl(screenPath)
+                ScreenUrlInfo sui = screenUrlInstance.sui
+                if (!screenUrlInstance.isPermitted()) { ec.logger.warn("View not permitted for path ${screenUrlInstance.url}") }
+                // build this subscreen's pathWithParams
+                String pathWithParams = "/" + sui.preTransitionPathNameList.join("/")
+                Map<String, String> parmMap = screenUrlInstance.getParameterMap()
+                // check for missing required parameters
+                boolean parmMissing = false
+                for (ScreenDefinition.ParameterItem pi in sui.pathParameterItems.values()) {
+                    if (!pi.required) continue
+                    String parmValue = parmMap.get(pi.name)
+                    if (parmValue == null || parmValue.isEmpty()) { parmMissing = true; break }
+                }
+                // if there is a parameter missing skip the subscreen
+                if (parmMissing) continue
+                String parmString = screenUrlInstance.getParameterString()
+                if (!parmString.isEmpty()) pathWithParams += ('?' + parmString)
+
+                String image = sui.menuImage
+                String imageType = sui.menuImageType
+                if (image != null && !image.isEmpty() && (imageType == null || imageType.isEmpty() || "url-screen".equals(imageType)))
+                    image = buildUrl(image).url
+
+                boolean active = (nextItem == subscreensItem.name)
+                Map itemMap = [name:subscreensItem.name, title:ec.resource.expand(subscreensItem.menuTitle, ""),
+                               path:screenPath, pathWithParams:pathWithParams, image:image, imageType:imageType]
+                if (subscreensItem.menuInclude) itemMap.menuInclude = true
+                if (active) itemMap.active = true
+                if (screenUrlInstance.disableLink) itemMap.disableLink = true
+                subscreensList.add(itemMap)
+                // not needed: screenStatic:sui.targetScreen.isServerStatic(renderMode)
+            }
+
+            String curScreenPath = currentPath.toString()
+            UrlInstance curUrlInstance = buildUrl(curScreenPath)
+            String curPathWithParams = curScreenPath
+            String curParmString = curUrlInstance.getParameterString()
+            if (!curParmString.isEmpty()) curPathWithParams = curPathWithParams + '?' + curParmString
+
+            ScreenUrlInfo sui = curUrlInstance.sui
+            String image = sui.menuImage
+            String imageType = sui.menuImageType
+            if (image != null && !image.isEmpty() && (imageType == null || imageType.isEmpty() || "url-screen".equals(imageType)))
+                image = buildUrl(image).url
+            String menuTitle = ec.l10n.localize(curSsi.menuTitle) ?: curScreen.getDefaultMenuName()
+
+            menuDataList.add([name:pathItem, title:menuTitle, subscreens:subscreensList, path:curScreenPath,
+                              pathWithParams:curPathWithParams, hasTabMenu:curScreen.hasTabMenu(), renderModes:curScreen.renderModes, image:image, imageType:imageType])
+            // not needed: screenStatic:curScreen.isServerStatic(renderMode)
+        }
+
+        String lastPathItem = (String) fullPathList.get(fullPathSize - 1)
+        fullUrlInstance.addParameters(ec.web.getRequestParameters())
+        currentPath.append('/').append(StringUtilities.urlEncodeIfNeeded(lastPathItem))
+        String lastPath = currentPath.toString()
+        String paramString = fullUrlInstance.getParameterString()
+        if (paramString.length() > 0) currentPath.append('?').append(paramString)
+
+        String lastImage = fullUrlInfo.menuImage
+        String lastImageType = fullUrlInfo.menuImageType
+        if (lastImage != null && !lastImage.isEmpty() && (lastImageType == null || lastImageType.isEmpty() || "url-screen".equals(lastImageType)))
+            lastImage = buildUrl(lastImage).url
+
+        SubscreensItem lastSsi = curScreen.getSubscreensItem(lastPathItem)
+        String lastTitle = ec.l10n.localize(lastSsi?.menuTitle) ?: fullUrlInfo.targetScreen.getDefaultMenuName()
+        if (lastTitle.contains('${')) lastTitle = ec.resourceFacade.expand(lastTitle, "")
+        List<Map<String, Object>> screenDocList = fullUrlInfo.targetScreen.getScreenDocumentInfoList()
+
+        // look for form-list with saved find on target screen, if so look for saved finds available to user to display in menu
+        List<Map> savedFindsList = new LinkedList<>()
+        ScreenDefinition targetScreen = fullUrlInfo.getTargetScreen()
+        ArrayList<ScreenForm> formList = targetScreen.getAllForms()
+        for (int i = 0; i < formList.size(); i++) {
+            ScreenForm screenForm = (ScreenForm) formList.get(i)
+            if (screenForm.isFormList && "true".equals(screenForm.internalFormNode.attribute("saved-finds"))) {
+                // is a saved find active (or has default)?
+                String formListFindId = ec.contextStack.getByString("formListFindId")
+                if (formListFindId == null || formListFindId.isEmpty()) formListFindId = screenForm.getUserDefaultFormListFindId(ec)
+
+                // add data for saved finds
+                List<Map<String, Object>> userFlfList = screenForm.getUserFormListFinds(ec)
+                for (Map<String, Object> userFlf in userFlfList) {
+                    EntityValue formListFind = (EntityValue) userFlf.formListFind
+                    Map itemMap = [name:formListFind.formListFindId, title:formListFind.description, image:lastImage, imageType:lastImageType,
+                                   path:lastPath, pathWithParams:(lastPath + "?formListFindId=" + formListFind.formListFindId)]
+                    if (formListFindId != null && formListFindId.equals(formListFind.formListFindId)) itemMap.active = true
+                    savedFindsList.add(itemMap)
+                }
+            }
+        }
+
+        if (extraPathList != null) {
+            int extraPathListSize = extraPathList.size()
+            for (int i = 0; i < extraPathListSize; i++) extraPathList.set(i, StringUtilities.urlEncodeIfNeeded((String) extraPathList.get(i)))
+        }
+        Map lastMap = [name:lastPathItem, title:lastTitle, path:lastPath, pathWithParams:currentPath.toString(),
+                       image:lastImage, imageType:lastImageType, extraPathList:extraPathList, screenDocList:screenDocList,
+                       renderModes:fullUrlInfo.targetScreen.renderModes, savedFinds:savedFindsList]
+        menuDataList.add(lastMap)
+        // not needed: screenStatic:fullUrlInfo.targetScreen.isServerStatic(renderMode)
+
+        // for (Map info in menuDataList) logger.warn("menu data item: ${info}")
+        return menuDataList
+    }
 }
